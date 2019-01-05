c12517bbd2e8e4812d443a486498b3cb15ab560e
Dave Astels <dastels@daveastels.com>
2019-01-04 19:46:13 -0500

    Fix indent error

--- adafruit_debouncer.py
+++ adafruit_debouncer.py
@@ -25,7 +25,7 @@
 
 Debounces an arbitrary predicate function (typically created as a lambda) of 0 arguments.
 Since a very common use is debouncing a digital input pin, the initializer accepts a pin number
- instead of a lambda.
+instead of a lambda.
 
 * Author(s): Dave Astels
 
